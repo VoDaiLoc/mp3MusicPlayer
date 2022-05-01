3a9c534c5d040c1558b10b1185522bd2cd00fe44
VoDaiLoc <vodailoc.kt@gmail.com>
2022-05-01 16:07:41 +0700

    update

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 
+
 class Music {
     constructor(id, image, songName, singer, musicFile) {
         this.id = id;
@@ -19,18 +20,8 @@ var musics = [
 
 var newMusics = [];
 
-let song = document.querySelector('#song');
-let indexSong = 0;
-song.setAttribute("src", `musics/${musics[indexSong]}`);
-let playbtn = document.querySelector('.play-pause');
-let backbtn = document.querySelector('.play-back');
-let forwardbtn = document.querySelector('.play-forward');
-let notPlaying = true;
 
 
-let durationTime = document.querySelector('.duration-time');
-let presentTime = document.querySelector('.present-time');
-let rangeBar = document.querySelector('.range');
 
 function renderListSong() {
     let htmls = musics.map(function (music, index) {
@@ -42,8 +33,7 @@ function renderListSong() {
                 <h3>${music.songName}</h3>
                 <h5>${music.singer}</h5>
             </div>
-            <div class="list-check${index} check-hidden"><ion-icon name="checkmark-circle-outline" class="ion-icon"></ion-icon></div>
-            <div class="list-add"><ion-icon name="add-circle-outline" class="ion-icon" onclick="addPlaylist(${index})"></ion-icon></div>
+            <div class="list-add${index}"><ion-icon name="add-circle-outline" class="ion-icon" onclick="addPlaylist(${index})"></ion-icon></div>
             </div>
         `
     })
@@ -68,93 +58,97 @@ function renderListPlaying() {
 }
 
 
-// function displayMusicPlayer() {
-//     let htmls = newMusics(function(music,index) {
-//         return `
-//             <div class="music-image">
-//                 <img src="images/${music.image}" alt="music-image">
-//             </div>
-//             <div class="textName">
-//                 <h3 class="music-name">${music.songName}</h3>
-//                 <p class="singer-name">Trình bày: ${singer}</p>
-//             </div>
-//             <div class="range-group">
-//                 <input type="range" class="range" onchange="changeRangeBar()">
-//                 <audio src="musics/${music.musicFile}" id="song" onended="endSong()"></audio>
-//             </div>
-//             <div class="times">
-//                 <div class="present-time"></div>
-//                 <div class="duration-time"></div>
-//             </div>
-//             <div class="controls">
-//                 <ion-icon name="shuffle" class="ion-icon"></ion-icon>
-//                 <ion-icon name="play-back" class="ion-icon play-back" onclick="playBack()"></ion-icon>
-//                 <div class="play-pause">
-//                     <ion-icon name="play" class="ion-icon" onclick="playPause()" id="play"></ion-icon>
-//                 </div>
-//                 <ion-icon name="play-forward" class="ion-icon play-forward" onclick="playForward()"></ion-icon>
-//                 <ion-icon name="repeat" class="ion-icon"></ion-icon>
-//             </div>
-//             <div class="action">
-//                 <div class="addMusic">
-//                     <button type="button" class="btn btn-primary" onclick="openListSong()">DANH SÁCH BÀI HÁT</button>
-//                 </div>
-//                 <div class="list">
-//                     <span id="list-sp">DANH SÁCH PHÁT</span>
-//                     <ion-icon name="menu" class="ion-icon" id="icon-menu" onclick="openPlayList()"></ion-icon>
-//                 </div>
-//             </div>
-//         `
-//     })
-//     document.querySelector('.layout-between').innerHTML = htmls.join('');
-// }
 
 
-function playPause() {
-    if (notPlaying) {
-        song.play();
-        notPlaying = false;
-        playbtn.innerHTML = `<ion-icon name="pause" class="ion-icon" onclick="playPause()"></ion-icon>`;
+function displayMusicPlayer() {
+    let htmls = newMusics.map(function (music, index) {
+        return `
+            <div class="music-image">
+                <img src="images/${music.image}" alt="music-image">
+            </div>
+            <div class="textName">
+                <h3 class="music-name">${music.songName}</h3>
+                <p class="singer-name">Trình bày: ${music.singer}</p>
+            </div>
+            <div class="range-group">
+                <input type="range" class="range" onchange="changeRangeBar()">
+                <audio src="musics/${music.musicFile}" id="song" onended="endSong()"></audio>
+            </div>
+            <div class="times">
+                <div class="present-time"></div>
+                <div class="duration-time"></div>
+            </div>
+            <div class="controls">
+                <ion-icon name="shuffle" class="ion-icon"></ion-icon>
+                <ion-icon name="play-back" class="ion-icon play-back" onclick="playBack()"></ion-icon>
+                <div class="play-pause">
+                    <ion-icon name="play" class="ion-icon" onclick="playPause()" id="play"></ion-icon>
+                </div>
+                <ion-icon name="play-forward" class="ion-icon play-forward" onclick="playForward()"></ion-icon>
+                <ion-icon name="repeat" class="ion-icon"></ion-icon>
+            </div>
+            <div class="action">
+                <div class="addMusic">
+                    <button type="button" class="btn btn-primary" onclick="openListSong()">DANH SÁCH BÀI HÁT</button>
+                </div>
+                <div class="list">
+                    <span id="list-sp">DANH SÁCH PHÁT</span>
+                    <ion-icon name="menu" class="ion-icon" id="icon-menu" onclick="openPlayList()"></ion-icon>
+                </div>
+            </div>
+        `
+    })
+    let htmlsDefault = `
+        <div class="music-image">
+        <img src="images/nodisc.png" alt="music-image">
+        </div>
+        <div class="textName">
+            <h5 class="music-name">VUI LÒNG THÊM BÀI HÁT VÀO DANH SÁCH PHÁT</h5>
+        </div>
+        <div class="range-group">
+            <input type="range" class="range" onchange="changeRangeBar()">
+            <audio src="" id="song" onended="endSong()"></audio>
+        </div>
+        <div class="times">
+            <div class="present-time"></div>
+            <div class="duration-time"></div>
+        </div>
+        <div class="controls">
+            <ion-icon name="shuffle" class="ion-icon"></ion-icon>
+            <ion-icon name="play-back" class="ion-icon play-back" onclick="playBack()"></ion-icon>
+            <div class="play-pause">
+                <ion-icon name="play" class="ion-icon" onclick="playPause()" id="play"></ion-icon>
+            </div>
+            <ion-icon name="play-forward" class="ion-icon play-forward" onclick="playForward()"></ion-icon>
+            <ion-icon name="repeat" class="ion-icon"></ion-icon>
+        </div>
+        <div class="action">
+            <div class="addMusic">
+                <button type="button" class="btn btn-primary" onclick="openListSong()">DANH SÁCH BÀI HÁT</button>
+            </div>
+            <div class="list">
+                <span id="list-sp">DANH SÁCH PHÁT</span>
+                <ion-icon name="menu" class="ion-icon" id="icon-menu" onclick="openPlayList()"></ion-icon>
+            </div>
+        </div>
+            `
+    if (newMusics.length > 0) {
+        document.querySelector('.layout-between').innerHTML = htmls.join('');
     }
     else {
-        song.pause();
-        notPlaying = true;
-        playbtn.innerHTML = `<ion-icon name="play" class="ion-icon" onclick="playPause()" id="play"></ion-icon>`;
+        document.querySelector('.layout-between').innerHTML = htmlsDefault;
     }
 }
 
 
-function playForward() {
-    indexSong++;
-    if (indexSong >= musics.length) {
-        indexSong = 0;
-        song.setAttribute("src", `musics/${musics[indexSong]}`);
-        notPlaying = true;
-        playPause();
-
-    }
-    else {
-        song.setAttribute("src", `musics/${musics[indexSong]}`);
-        notPlaying = true;
-        playPause();
-    }
-
-}
-
-
-function playBack() {
-    indexSong--;
-    if (indexSong < 0) {
-        indexSong = musics.length - 1;
-        song.setAttribute("src", `musics/${musics[indexSong]}`);
-        notPlaying = true;
-        playPause();
-    }
-    song.setAttribute("src", `musics/${musics[indexSong]}`);
-    notPlaying = true;
-    playPause();
-}
-
+let song = document.querySelector('#song');
+let indexSong = 0;
+let playbtn = document.querySelector('.play-pause');
+let backbtn = document.querySelector('.play-back');
+let forwardbtn = document.querySelector('.play-forward');
+let durationTime = document.querySelector('.duration-time');
+let presentTime = document.querySelector('.present-time');
+let rangeBar = document.querySelector('.range');
 
 function displayTime() {
     rangeBar.max = song.duration;
@@ -174,6 +168,57 @@ function formatTime(time) {
     let seconds = Math.floor(time - minutes * 60);
     return `${minutes}:${seconds}`;
 }
+
+
+
+let notPlaying = true;
+function playPause() {
+    if (notPlaying) {
+        song.play();
+        notPlaying = false;
+        playbtn.innerHTML = `<ion-icon name="pause" class="ion-icon" onclick="playPause()"></ion-icon>`;
+    }
+    else {
+        song.pause();
+        notPlaying = true;
+        playbtn.innerHTML = `<ion-icon name="play" class="ion-icon" onclick="playPause()" id="play"></ion-icon>`;
+    }
+}
+
+
+function playForward() {
+    indexSong++;
+    if (indexSong >= musics.length) {
+        indexSong = 0;
+
+        notPlaying = true;
+        playPause();
+
+    }
+    else {
+
+        notPlaying = true;
+        playPause();
+    }
+
+}
+
+
+function playBack() {
+    indexSong--;
+    if (indexSong < 0) {
+        indexSong = musics.length - 1;
+
+        notPlaying = true;
+        playPause();
+    }
+
+    notPlaying = true;
+    playPause();
+}
+
+
+
 
 function changeRangeBar() {
     song.currentTime = rangeBar.value;
@@ -208,7 +253,7 @@ function addPlaylist(index) {
     })
     if (!checkMusic) {
         newMusics.push(musics[index]);
-        document.querySelector(`.list-check${index}`).classList.remove("check-hidden");
+        document.querySelector(`.list-add${index}`).classList.add("add-hidden");
         renderListSong();
         renderListPlaying();
         displayMusicPlayer();
@@ -223,10 +268,11 @@ function removeSong(index) {
     if (confirmed) {
         newMusics.splice(index, 1);
         renderListPlaying();
+        displayMusicPlayer();
     }
 }
-// displayTime();
-// setInterval(displayTime,500);
+displayTime();
+setInterval(displayTime, 500);
 renderListSong();
 renderListPlaying();
 displayMusicPlayer();
