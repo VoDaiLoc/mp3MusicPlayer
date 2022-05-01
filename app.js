385aaae707be4a6aa70c8e856b6d4676d753a140
VoDaiLoc <vodailoc.kt@gmail.com>
2022-05-01 12:46:52 +0700

    upload case

---
+++ app.js
@@ -0,0 +1,232 @@
+
+class Music {
+    constructor(id, image, songName, singer, musicFile) {
+        this.id = id;
+        this.image = image;
+        this.songName = songName;
+        this.singer = singer;
+        this.musicFile = musicFile;
+    }
+}
+
+var musics = [
+    new Music(1, 'lalatran.jpg', 'Anh Yêu Vội Thế - Remix', 'Lala Trần', 'AnhYeuVoiThe.mp3'),
+    new Music(2, 'thientu.jpg', 'Cô Độc Vương - Remix', 'Thiên Tú', 'CoDocVuongRemix.mp3'),
+    new Music(3, 'dunghoangpham.jpg', 'Đế Vương - Remix', 'Dung Hoàng Phạm'),
+    new Music(4, 'xuanduc.jpg', 'Mãi Mãi Là Bao Lâu - ReMix', 'Xuân Đức', 'DeVuongRemix.mp3'),
+    new Music(5, 'tangduytan.jpg', 'Ngây Thơ', 'Tăng Duy Tân', 'NgayTho.mp3')
+]
+
+var newMusics = [];
+
+let song = document.querySelector('#song');
+let indexSong = 0;
+song.setAttribute("src", `musics/${musics[indexSong]}`);
+let playbtn = document.querySelector('.play-pause');
+let backbtn = document.querySelector('.play-back');
+let forwardbtn = document.querySelector('.play-forward');
+let notPlaying = true;
+
+
+let durationTime = document.querySelector('.duration-time');
+let presentTime = document.querySelector('.present-time');
+let rangeBar = document.querySelector('.range');
+
+function renderListSong() {
+    let htmls = musics.map(function (music, index) {
+        return `
+            <div class="lists">
+            <div class="list-icon"><ion-icon name="musical-notes"></ion-icon></div>
+            <div class="list-image"><img src="images/${music.image}" alt="image${music.id}" class="avatar"></div>
+            <div class="list-title">
+                <h3>${music.songName}</h3>
+                <h5>${music.singer}</h5>
+            </div>
+            <div class="list-check${index} check-hidden"><ion-icon name="checkmark-circle-outline" class="ion-icon"></ion-icon></div>
+            <div class="list-add"><ion-icon name="add-circle-outline" class="ion-icon" onclick="addPlaylist(${index})"></ion-icon></div>
+            </div>
+        `
+    })
+    document.querySelector('.list-song').innerHTML = htmls.join('');
+}
+
+function renderListPlaying() {
+    let htmls = newMusics.map(function (music, index) {
+        return `
+            <div class="lists">
+            <div class="list-icon"><ion-icon name="musical-notes"></ion-icon></div>
+            <div class="list-image"><img src="images/${music.image}" alt="image${music.id}" class="avatar"></div>
+            <div class="list-title">
+                <h3>${music.songName}</h3>
+                <h5>${music.singer}</h5>
+            </div>
+            <div class="list-remove"><ion-icon name="trash-outline" class="ion-icon" onclick="removeSong(${index})"></ion-icon></div>
+            </div>
+        `
+    })
+    document.querySelector('.list-playing').innerHTML = htmls.join('');
+}
+
+
+// function displayMusicPlayer() {
+//     let htmls = newMusics(function(music,index) {
+//         return `
+//             <div class="music-image">
+//                 <img src="images/${music.image}" alt="music-image">
+//             </div>
+//             <div class="textName">
+//                 <h3 class="music-name">${music.songName}</h3>
+//                 <p class="singer-name">Trình bày: ${singer}</p>
+//             </div>
+//             <div class="range-group">
+//                 <input type="range" class="range" onchange="changeRangeBar()">
+//                 <audio src="musics/${music.musicFile}" id="song" onended="endSong()"></audio>
+//             </div>
+//             <div class="times">
+//                 <div class="present-time"></div>
+//                 <div class="duration-time"></div>
+//             </div>
+//             <div class="controls">
+//                 <ion-icon name="shuffle" class="ion-icon"></ion-icon>
+//                 <ion-icon name="play-back" class="ion-icon play-back" onclick="playBack()"></ion-icon>
+//                 <div class="play-pause">
+//                     <ion-icon name="play" class="ion-icon" onclick="playPause()" id="play"></ion-icon>
+//                 </div>
+//                 <ion-icon name="play-forward" class="ion-icon play-forward" onclick="playForward()"></ion-icon>
+//                 <ion-icon name="repeat" class="ion-icon"></ion-icon>
+//             </div>
+//             <div class="action">
+//                 <div class="addMusic">
+//                     <button type="button" class="btn btn-primary" onclick="openListSong()">DANH SÁCH BÀI HÁT</button>
+//                 </div>
+//                 <div class="list">
+//                     <span id="list-sp">DANH SÁCH PHÁT</span>
+//                     <ion-icon name="menu" class="ion-icon" id="icon-menu" onclick="openPlayList()"></ion-icon>
+//                 </div>
+//             </div>
+//         `
+//     })
+//     document.querySelector('.layout-between').innerHTML = htmls.join('');
+// }
+
+
+function playPause() {
+    if (notPlaying) {
+        song.play();
+        notPlaying = false;
+        playbtn.innerHTML = `<ion-icon name="pause" class="ion-icon" onclick="playPause()"></ion-icon>`;
+    }
+    else {
+        song.pause();
+        notPlaying = true;
+        playbtn.innerHTML = `<ion-icon name="play" class="ion-icon" onclick="playPause()" id="play"></ion-icon>`;
+    }
+}
+
+
+function playForward() {
+    indexSong++;
+    if (indexSong >= musics.length) {
+        indexSong = 0;
+        song.setAttribute("src", `musics/${musics[indexSong]}`);
+        notPlaying = true;
+        playPause();
+
+    }
+    else {
+        song.setAttribute("src", `musics/${musics[indexSong]}`);
+        notPlaying = true;
+        playPause();
+    }
+
+}
+
+
+function playBack() {
+    indexSong--;
+    if (indexSong < 0) {
+        indexSong = musics.length - 1;
+        song.setAttribute("src", `musics/${musics[indexSong]}`);
+        notPlaying = true;
+        playPause();
+    }
+    song.setAttribute("src", `musics/${musics[indexSong]}`);
+    notPlaying = true;
+    playPause();
+}
+
+
+function displayTime() {
+    rangeBar.max = song.duration;
+    rangeBar.value = song.currentTime;
+    if (!song.duration) {
+        presentTime.textContent = "00:00";
+        durationTime.textContent = "00:00";
+    }
+    else {
+        presentTime.textContent = formatTime(song.currentTime);
+        durationTime.textContent = formatTime(song.duration);
+    }
+}
+
+function formatTime(time) {
+    let minutes = Math.floor(time / 60);
+    let seconds = Math.floor(time - minutes * 60);
+    return `${minutes}:${seconds}`;
+}
+
+function changeRangeBar() {
+    song.currentTime = rangeBar.value;
+}
+
+
+function endSong() {
+    playForward();
+}
+
+
+function openListSong() {
+    document.querySelector('.layout-left').classList.remove('ll-hidden');
+}
+
+function openPlayList() {
+    document.querySelector('.layout-right').classList.remove('lr-hidden');
+}
+
+
+function closeListSong() {
+    document.querySelector('.layout-left').classList.add('ll-hidden');
+}
+
+function closePlayList() {
+    document.querySelector('.layout-right').classList.add('lr-hidden');
+}
+
+function addPlaylist(index) {
+    let checkMusic = newMusics.find(function (music) {
+        return music.id == index + 1;
+    })
+    if (!checkMusic) {
+        newMusics.push(musics[index]);
+        document.querySelector(`.list-check${index}`).classList.remove("check-hidden");
+        renderListSong();
+        renderListPlaying();
+        displayMusicPlayer();
+    }
+    else {
+        alert('Bài hát này đã có trong danh sách phát rồi');
+    }
+}
+
+function removeSong(index) {
+    let confirmed = window.confirm(`Bạn chắc chắn muốn xóa bài hát: ${newMusics[index].songName} ra khỏi danh sách phát?`);
+    if (confirmed) {
+        newMusics.splice(index, 1);
+        renderListPlaying();
+    }
+}
+// displayTime();
+// setInterval(displayTime,500);
+renderListSong();
+renderListPlaying();
+displayMusicPlayer();
